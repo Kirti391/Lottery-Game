7517f64c3f29c7bd096706748ae041bf480f9833
Kirti <162712321+Kirti391@users.noreply.github.com>
2026-08-17 19:55:26 +0530

    feat: Ticket with random number created

--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,7 +1,18 @@
+import {useState} from 'react'
+import "./Lottery.css"
+import { genTicket } from './helper';
+
 export default function Lottery() {
+    let [ticket,setTicket]=useState(genTicket(3));
   return (
     <div>
       <h1>Lottery</h1>
+      <div className='ticket'> 
+        <span>{ticket[0]}</span>
+        <span>{ticket[0]}</span>
+        <span>{ticket[0]}</span>
+        </div>
+     
     </div>
   )
 }
