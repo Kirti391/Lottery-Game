e4bc9ff79182178b25795b001888d08aadaddf92
Kirti <162712321+Kirti391@users.noreply.github.com>
2026-08-31 21:26:55 +0530

    feat: Winning ticket condition added

--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,18 +1,25 @@
 import {useState} from 'react'
 import "./Lottery.css"
-import { genTicket } from './helper';
+import { genTicket, sum } from './helper';
 
 export default function Lottery() {
     let [ticket,setTicket]=useState(genTicket(3));
+    let isWinning=sum(ticket)===15;
+
+    let buyTicket=()=>{
+      setTicket(genTicket(3));
+    }
   return (
     <div>
       <h1>Lottery</h1>
       <div className='ticket'> 
         <span>{ticket[0]}</span>
-        <span>{ticket[0]}</span>
-        <span>{ticket[0]}</span>
+        <span>{ticket[1]}</span>
+        <span>{ticket[2]}</span>
         </div>
-     
+        <br />
+        <button onClick={buyTicket}>Buy New Ticket</button>
+     <h3>{isWinning && "Congratulations , You Won !!!"}</h3>
     </div>
   )
 }
