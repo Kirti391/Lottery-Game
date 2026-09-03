e2bd61c08da8ab93def5477fa211f690d10f707c
Kirti <162712321+Kirti391@users.noreply.github.com>
2026-09-03 22:22:21 +0530

    Feat: TicketNum component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Lottery from './Lottery'
 import './App.css'
-
+import TicketNum from './TicketNum'
 function App() {
   
 
   return (
     <>
       <Lottery />
+      <TicketNum num="6"/>
+      <TicketNum num="4"/>
+      <TicketNum num="2"/>
     </>
   )
 }
