35a0e87f771a6fba0bc02a9b41b89b83ba0f0c3e
Kirti <162712321+Kirti391@users.noreply.github.com>
2026-09-10 23:53:34 +0530

    feat: styling improved

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { sum } from './helper'
 function App() {
    let winCondition=(ticket)=>{
     // return sum(ticket)===15;
-    return ticket.every((num)=>num===ticket[0]);
+    // return ticket.every((num)=>num===ticket[0]);
+    return ticket[0]===0;
    }
 
   return (
